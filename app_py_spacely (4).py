8158d9e0da5a8a0eddbe2d701f47727a1579111d
Ertyuuu55 <gunawaneric104@gmail.com>
2025-12-26 09:39:41 +0700

    Update app_py_spacely (4).py

--- app_py_spacely (4).py
+++ app_py_spacely (4).py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1dZbL18Fy4RfuMLLH2NmMgaAUWJzOAALK
 """
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 
